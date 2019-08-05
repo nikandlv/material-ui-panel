33cab1c4bfdd4395e3f4cfaced9569ac68974b29
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-05 23:00:39 +0430

    Added cssbaseline to application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'typeface-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
     <div className="App">
-      
+      <CssBaseline />
+            
     </div>
   );
 }
