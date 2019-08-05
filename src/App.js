4d9d1d2133d9dbdb8d71f61ee3b62e958bb69d7b
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-05 23:15:18 +0430

    Added a base for the panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'typeface-roboto';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
+import Appbar from './layouts/Appbar'
+import ResponsiveDrawer from './layouts/Drawer';
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-            
+      <ResponsiveDrawer />
     </div>
   );
 }
