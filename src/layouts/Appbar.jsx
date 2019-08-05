aa37f48b64be952232efaa8add27ee7278c26c6f
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-05 23:19:43 +0430

    Removed the searchbar

--- src/layouts/Appbar.jsx
+++ src/layouts/Appbar.jsx
@@ -187,19 +187,6 @@ export default function PrimarySearchAppBar(props) {
           <Typography className={classes.title} variant="h6" noWrap>
             Material-Panel
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <IconButton aria-label="show 4 new mails" color="inherit">
