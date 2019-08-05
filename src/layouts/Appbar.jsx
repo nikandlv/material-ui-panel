e237a87ddf942c63f779727c54b784000d5c573d
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-05 23:20:43 +0430

    Removed more unused styles

--- src/layouts/Appbar.jsx
+++ src/layouts/Appbar.jsx
@@ -32,21 +32,6 @@ const useStyles = makeStyles(theme => ({
       display: 'block',
     },
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
