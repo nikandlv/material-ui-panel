5ff094cc34331b4a11e3866507b8d800fdc9f029
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-06 16:09:32 +0430

    Removed redundent code

--- src/layouts/Drawer.jsx
+++ src/layouts/Drawer.jsx
@@ -285,12 +285,5 @@ function ResponsiveDrawer(props) {
   );
 }
 
-ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  container: PropTypes.object,
-};
 
 export default ResponsiveDrawer;
