220dc51879aabea7228424d44f36ddd32d74ff1b
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-18 23:58:53 +0430

    Added a better baseline for settings and removed unused import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Paper, Typography, CardContent, Card } from '@material-ui/core';
+import { Typography, CardContent, Card } from '@material-ui/core';
 export default function About() {
     return (
         <div>
