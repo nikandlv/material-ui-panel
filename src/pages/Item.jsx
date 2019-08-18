3de3eb5cf63a8da55b3eb195b2a0d15a5149a9c4
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-18 23:59:35 +0430

    Added a better item page

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -1,8 +1,14 @@
+
 import React from 'react'
+import { Typography, CardContent, Card } from '@material-ui/core';
 export default function Item(props) {
     return (
         <div>
-            Item {props.number}
+            <Card>
+                <CardContent>
+                    <Typography variant="h5">Item {props.number}</Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
