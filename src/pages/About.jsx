1e3343098761d22c4b376172fbbcfa8037fdb7ca
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-18 23:58:11 +0430

    Added a better about page baseline

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Paper, Typography, CardContent, Card } from '@material-ui/core';
 export default function About() {
     return (
         <div>
-            About
+            <Card>
+                <CardContent>
+                    <Typography variant="h5">About</Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
